bf2717a7f300b76d8f66ac9ede3d2888c71992b6
c5261499 <ambar.bhatnagar@sap.com>
2021-01-11 02:50:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ function App() {
   return (
     <form className="widget-form">
       <div>
-        <img className="form-image form-spacing" src="https://miro.medium.com/max/1400/1*MI686k5sDQrISBM6L8pf5A.jpeg"></img>
+        <img className="form-image form-spacing" src="https://miro.medium.com/max/1400/1*MI686k5sDQrISBM6L8pf5A.jpeg"
+        alt="Relevant textual alternative to the image" ></img>
       </div>
       <div>
         <input className="form-spacing" type="text" name="name" placeholder="Enter Email" />
