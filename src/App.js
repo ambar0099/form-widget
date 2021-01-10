9f163d42ccac9eb0b582a7d21dd30a1918d2d26d
c5261499 <ambar.bhatnagar@sap.com>
2021-01-11 01:37:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -3,22 +3,17 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form className="widget-form">
+      <div>
+        <img className="form-image form-spacing" src="https://miro.medium.com/max/1400/1*MI686k5sDQrISBM6L8pf5A.jpeg"></img>
+      </div>
+      <div>
+        <input className="form-spacing" type="text" name="name" placeholder="Enter Email" />
+      </div>
+      <div className="form-spacing">
+        <input type="submit" value="Submit" />
+      </div>
+    </form>
   );
 }
 
